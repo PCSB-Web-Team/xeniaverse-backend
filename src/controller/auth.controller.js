30ac7bd4d86702df3c7a23bacfac7a71c9dffcf1
atharvakinikar <atharvapradeeprao.k20@iiits.in>
2022-02-09 11:59:09 +0530

    passeword reset link

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -101,8 +101,7 @@ async function forgotLink(req, res) {
       from: "pictpbl@gmail.com",
       to: email,
       subject: "One time password reset",
-      text: "We have sent you this email in response to your request to reset your password on ${site-name}. After you reset your password, any credit card information stored in My Account will be deleted as a security measure.  To reset your password for ${site-url}, please follow the link below:  ${https://xeniaverse.co.in/resetpassword/${user._id}/${token}}   We recommend that you keep your password secure and not share it with anyone. If you feel your password has been compromised, you can change it by going to your My Account Page and clicking on the Change Email Address or Password link.",
-      // html: `<h1>Password reset link</h1><a href=${`https://xeniaverse.co.in/resetpassword/${user._id}/${token}`}>Password reset</a>`,
+      html: `<h1>Password reset link</h1><a href=${`https://xeniaverse.co.in/resetpassword/${user._id}/${token}`}>Password reset</a>`,
     };
     mailTransporter.sendMail(details);
     res.send("link has been sent");
